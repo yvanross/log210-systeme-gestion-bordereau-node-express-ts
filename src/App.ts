864a256e57fe6f6eae2005234cd70f298799e877
Paul Maréchal <marechap.info@gmail.com>
2020-10-09 10:29:41 -0400

    run prettier

--- src/App.ts
+++ src/App.ts
@@ -6,49 +6,45 @@ import * as bodyParser from 'body-parser';
 import { sgbRoutes } from './routes/SgbRouter';
 import { sgbRoutesV2 } from './routes/SgbRouterV2';
 
-
 // Creates and configures an ExpressJS web server.
 class App {
+    // ref to Express instance
+    public express: express.Application;
 
-  // ref to Express instance
-  public express: express.Application;
+    //Run configuration methods on the Express instance.
+    constructor() {
+        this.express = express();
+        this.middleware();
+        this.routes();
+    }
 
-  //Run configuration methods on the Express instance.
-  constructor() {
-    this.express = express();
-    this.middleware();
-    this.routes();
-  }
+    // Configure Express middleware.
+    private middleware(): void {
+        this.express.use(logger('dev'));
+        this.express.use(bodyParser.json());
+        this.express.use(bodyParser.urlencoded({ extended: false }));
+    }
 
-  // Configure Express middleware.
-  private middleware(): void {
-    this.express.use(logger('dev'));
-    this.express.use(bodyParser.json());
-    this.express.use(bodyParser.urlencoded({ extended: false }));
-  }
+    // Configure API endpoints.
+    private routes(): void {
+        /* This function will change when we start to add more
+         * API endpoints */
+        let router = express.Router();
+        // placeholder route handler
+        // router.get('/', (req, res, next) => {
+        //   res.json({
+        //     message: 'Bonjour monde!'
+        //   });
+        // });
 
-  // Configure API endpoints.
-  private routes(): void {
-    /* This function will change when we start to add more
-     * API endpoints */
-    let router = express.Router();
-    // placeholder route handler
-    // router.get('/', (req, res, next) => {
-    //   res.json({
-    //     message: 'Bonjour monde!'
-    //   });
-    // });
+        router.get('/', function (req, res) {
+            res.redirect('/docs/index.html');
+        });
 
-    router.get('/', function(req, res) {
-      res.redirect('/docs/index.html'); 
-  });
-
-    this.express.use('/', router);  // routage de base
-    this.express.use('/api/v1', sgbRoutes.router);  // tous les URI pour le scénario du système de gestion des bordereau commencent ainsi
-    this.express.use('/api/v2', sgbRoutesV2.router);  // tous les URI pour le scénario du système de gestion des bordereau commencent ainsi
-    this.express.use('/docs', express.static('dist/docs'))
- 
-  }
-
+        this.express.use('/', router); // routage de base
+        this.express.use('/api/v1', sgbRoutes.router); // tous les URI pour le scénario du système de gestion des bordereau commencent ainsi
+        this.express.use('/api/v2', sgbRoutesV2.router); // tous les URI pour le scénario du système de gestion des bordereau commencent ainsi
+        this.express.use('/docs', express.static('dist/docs'));
+    }
 }
 export default new App().express;
--- src/routes/SgbRouterV2.ts
+++ src/routes/SgbRouterV2.ts
@@ -2,49 +2,48 @@ import { Router, Request, Response, NextFunction } from 'express';
 import { SgbController } from '../core/SgbController';
 
 export class SgbRouterV2 {
-	router: Router;
-	controller: SgbController;  // contrôleur GRASP
-	router_latency: number;
+    router: Router;
+    controller: SgbController; // contrôleur GRASP
+    router_latency: number;
 
-	/**
-	* Initialize the Router
-	*/
-	constructor() {
-		this.router_latency = 0;
-		this.controller = new SgbController();  // init contrôleur GRASP
-		this.router = Router();
-		this.init();
-	}
+    /**
+     * Initialize the Router
+     */
+    constructor() {
+        this.router_latency = 0;
+        this.controller = new SgbController(); // init contrôleur GRASP
+        this.router = Router();
+        this.init();
+    }
 
-	
-	public login(req: Request, res: Response, next: NextFunction) { 
-		try {
-			// Invoquer l'opération système (du DSS) dans le contrôleur GRASP
-			let data = this.controller.loginV2(
-				req.query.email as string,
-				req.query.password as string);
-			// console.log("V2: login called with email: " + req.query.email + " and password")
-			// console.log("token = ",data[0], "user = ", data[1])
-			// console.log(data[1])
-			res.status(200)
-			.send({
-				message: 'Success',
-				status: res.status,
-				token: data[0],
-				user: data[1]
-			});
-		} catch (error) {
-			let code = 500; // internal server error
-			res.status(code).json({ error: error.toString() });
-		}
-	}
+    public login(req: Request, res: Response, next: NextFunction) {
+        try {
+            // Invoquer l'opération système (du DSS) dans le contrôleur GRASP
+            let data = this.controller.loginV2(
+                req.query.email as string,
+                req.query.password as string
+            );
+            // console.log("V2: login called with email: " + req.query.email + " and password")
+            // console.log("token = ",data[0], "user = ", data[1])
+            // console.log(data[1])
+            res.status(200).send({
+                message: 'Success',
+                status: res.status,
+                token: data[0],
+                user: data[1],
+            });
+        } catch (error) {
+            let code = 500; // internal server error
+            res.status(code).json({ error: error.toString() });
+        }
+    }
 
-	/**
-	* Take each handler, and attach to one of the Express.Router's
-	* endpoints.
-	*/
-	init() {
-	/**
+    /**
+     * Take each handler, and attach to one of the Express.Router's
+     * endpoints.
+     */
+    init() {
+        /**
 	 * @api {get} /v2/login?email=email&password=password Login
 	 * @apiGroup Application
 	 * @apiDescription Authentification de l'usager et récupération du token d'authentification
@@ -62,8 +61,8 @@ export class SgbRouterV2 {
   password: '' }
 	 */
 
-		this.router.get('/login', this.login.bind(this)); // pour .bind voir https://stackoverflow.com/a/15605064/1168342
-	}
+        this.router.get('/login', this.login.bind(this)); // pour .bind voir https://stackoverflow.com/a/15605064/1168342
+    }
 }
 
 // exporter its configured Express.Router
